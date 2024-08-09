853c45047b8397a9ebb319e01912a07d76939e9f
maayan_aytek <maayan.aytek@gmail.com>
2024-08-09 10:22:21 +0300

    changed ports, added mongo to requirements

--- web_server/main.py
+++ web_server/main.py
@@ -5,5 +5,5 @@ app = create_app()
 if __name__ == '__main__':
     # app.run(host='192.168.1.21', port=5000, debug=True)
     # socketio.run(app, debug=True)
-    socketio.run(app, debug=True, host='0.0.0.0', port=5050, use_reloader=False)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, use_reloader=False, allow_unsafe_werkzeug=True)
     # socketio.run(app, debug=True, host='0.0.0.0', port=5050, use_reloader=False, async_mode='eventlet')
